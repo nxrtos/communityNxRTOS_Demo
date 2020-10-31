874563ae72cc5c911a2a8454d42868c91f8372f7
Blue Now <bluenow1896@gmail.com>
2020-10-31 12:21:44 -0400

    drop in Packages used in the demo project BSP, CMSIS, STM32F0xx_HAL_Driver, STM32F4xx_HAL_Driver, and Audio Lib, USB lib from ST.

---
+++ SharedPacks/BSP/STM32F0-Discovery/bsp.h
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * File Name          : bsp.h
+  * Description        :
+  ******************************************************************************
+  *
+  ******************************************************************************
+  */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BSP_H
+#define __BSP_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0_discovery.h"
+#include "stm32f0xx_hal.h"
+// it is no need blow as  "stm32f0xx_hal_conf.h" will pick up right option,
+// but here for eliminate compiler false warning/error
+#include "stm32f0xx_hal_rcc.h"
+#include "stm32f0xx_hal_flash.h"
+#include "stm32f0xx_hal_cortex.h"
+#include "stm32f0xx_hal_gpio.h"
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+*/
+
+#endif /* __BSP_H */
---
+++ SharedPacks/BSP/STM32F0308-Discovery/stm32f0308_discovery.h
@@ -0,0 +1,213 @@
+/**
+  ******************************************************************************
+  * @file    stm32f0308_discovery.h
+  * @author  MCD Application Team
+  * @brief   This file contains definitions for STM32F0308-Discovery's Leds, push-
+  *          buttons hardware resources.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT(c) 2016 STMicroelectronics</center></h2>
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F0308_DISCOVERY_H
+#define __STM32F0308_DISCOVERY_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+#include "stdint.h"
+/** @addtogroup BSP
+  * @{
+  */
+
+/** @defgroup STM32F0308_DISCOVERY STM32F0308_DISCOVERY
+  * @{
+  */
+
+/** @defgroup STM32F0308_DISCOVERY_Common STM32F0308_DISCOVERY Common
+  * @{
+  */
+
+/** @defgroup STM32F0308_DISCOVERY_Exported_Types Exported Types
+  * @{
+  */
+
+/**
+ * @brief LED Types Definition
+ */
+typedef enum
+{
+  LED3 = 0,
+  LED4 = 1,
+  /* Color led aliases */
+  LED_GREEN  = LED3,
+  LED_BLUE   = LED4,
+  /* GLED defines */
+  GLED_1 = LED3,
+  GLED_2 = LED4,
+  //
+  LED_MIN	= 0,
+  LED_MAX	= 1,
+  LED_ALL	= 0xFF,
+
+  // define substitute
+  LED_RED	 = LED_BLUE,
+  LED_ORANGE = LED_BLUE,
+  LED5 = LED3,
+  LED6 = LED4,
+
+}Led_TypeDef;
+
+
+typedef enum 
+{  
+  BUTTON_MODE_GPIO = 0,
+  BUTTON_MODE_EXTI = 1,
+  BUTTON_MODE_RFEXTI = 2,
+
+  BUTTON_MODE_DEFAULT
+}ButtonMode_TypeDef;
+
+/**
+  * @}
+  */ 
+
+/** @defgroup STM32F0308_DISCOVERY_Exported_Constants Exported Constants
+  * @{
+  */ 
+/** 
+* @brief  Define for STM32F0308_DISCOVERY board  
+*/ 
+#if !defined (USE_STM320308_DISCO)
+ #define USE_STM320308_DISCO
+#endif
+
+/** @defgroup STM32F0308_DISCOVERY_LED STM32F0308_DISCOVERY LED
+  * @{
+  */
+#define LEDn                             2
+  
+#define LED3_PIN                         GPIO_PIN_9
+#define LED3_GPIO_PORT                   GPIOC
+#define LED3_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOC_CLK_ENABLE()
+#define LED3_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOC_CLK_DISABLE()
+  
+#define LED4_PIN                         GPIO_PIN_8
+#define LED4_GPIO_PORT                   GPIOC
+#define LED4_GPIO_CLK_ENABLE()           __HAL_RCC_GPIOC_CLK_ENABLE()
+#define LED4_GPIO_CLK_DISABLE()          __HAL_RCC_GPIOC_CLK_DISABLE()
+
+#define LEDx_GPIO_CLK_ENABLE(__LED__)   do { if((__LED__) == LED3) LED3_GPIO_CLK_ENABLE(); else \
+                                         if((__LED__) == LED4) LED4_GPIO_CLK_ENABLE();} while(0)
+
+#define LEDx_GPIO_CLK_DISABLE(__LED__)  (((__LED__) == LED3) ? LED3_GPIO_CLK_DISABLE() :\
+                                         ((__LED__) == LED4) ? LED4_GPIO_CLK_DISABLE() : 0 )
+/**
+  * @}
+  */ 
+
+/** @defgroup STM32F0308_DISCOVERY_BUTTON STM32F0308_DISCOVERY BUTTON
+  * @{
+  */  
+#define BUTTONn                          4
+
+/**
+ * @brief BUTTON Types Definition
+ */
+typedef enum
+{
+  BUTTON_USER = 0,
+  BUTTON_EXT1 = 1,
+  BUTTON_EXT2 = 2,
+  BUTTON_EXT3 = 3,
+
+  BUTTON_MIN = 0,
+  BUTTON_MAX = (BUTTONn -1),
+
+  BUTTON_ALL = 0x7f
+
+}Button_TypeDef;
+
+
+/**
+ * @brief USER push-button
+ */
+#define USER_BUTTON_PIN                GPIO_PIN_0                       /* PA0 */
+#define USER_BUTTON_GPIO_PORT          GPIOA
+#define USER_BUTTON_GPIO_CLK_ENABLE()  __HAL_RCC_GPIOA_CLK_ENABLE()
+#define USER_BUTTON_GPIO_CLK_DISABLE() __HAL_RCC_GPIOA_CLK_DISABLE()
+#define USER_BUTTON_EXTI_IRQn          EXTI0_1_IRQn 
+
+#define BUTTONx_GPIO_CLK_ENABLE(__BUTTON__)    do { if((__BUTTON__) == BUTTON_USER) USER_BUTTON_GPIO_CLK_ENABLE();} while(0)
+
+#define BUTTONx_GPIO_CLK_DISABLE(__BUTTON__)   (((__BUTTON__) == BUTTON_USER) ? USER_BUTTON_GPIO_CLK_DISABLE() : 0 )
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+  
+/** @defgroup STM32F0308_DISCOVERY_Exported_Functions Exported Functions
+  * @{
+  */
+uint32_t BSP_GetVersion(void);
+void     BSP_LED_Init(Led_TypeDef Led);
+void     BSP_LED_On(Led_TypeDef Led);
+void     BSP_LED_Off(Led_TypeDef Led);
+void     BSP_LED_Toggle(Led_TypeDef Led);
+void     BSP_PB_Init(Button_TypeDef Button, ButtonMode_TypeDef Mode);
+uint32_t BSP_PB_GetState(Button_TypeDef Button);
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F0308_DISCOVERY_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
